907d9b0c8149c56154779d117111263053ccd082
B0r1ss <borisenko.va90@gmail.com>
2021-03-14 02:25:21 +0300

    add js full

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-let likeButton=document.querySelector(".place__like-button")
-let profileButtonAdd=document.querySelectorAll(".profile__button_add")
-let profileButtonEdit=document.querySelectorAll(".profile__button_edit")
+let likeButtons=document.querySelectorAll(".place__like-button")
+let profileButtonAdd=document.querySelector(".profile__button_add")
+let profileButtonEdit=document.querySelector(".profile__button_edit")
+let closeButtonPopup=document.querySelector(".popup__close-button")
 
-console.log(likeButton)
-likeButton.addEventListener("click", function() {
-  likeButton.classList.toggle("place__like-button_enable")
+let popup=document.querySelector(".popup")
+let popupForm=document.querySelector(".popup__container")
+
+let username=document.querySelector(".profile__username")
+let position=document.querySelector(".profile__position")
+let inputUsername=document.querySelectorAll(".popup__input")[0]
+let inputPosition=document.querySelectorAll(".popup__input")[1]
+
+
+
+likeButtons.forEach(element =>{element.addEventListener("click", function() {
+  element.classList.toggle("place__like-button_enable")
+  })
 });
+
+profileButtonEdit.addEventListener("click", function() {
+  popup.classList.add("popup_opened")
+  inputUsername.setAttribute('value', username.textContent)
+  inputPosition.setAttribute('value', position.textContent)
+});
+
+closeButtonPopup.addEventListener("click", function() {
+  popup.classList.remove("popup_opened")
+});
+
+closeButtonPopup.addEventListener("click", function() {
+  popup.classList.remove("popup_opened")
+});
+
+console.log(username)
+
+popupForm.addEventListener("submit", function(evt) {
+  evt.preventDefault()
+  let popUsername=inputUsername.value
+  let popPosition=inputPosition.value
+  popup.classList.remove("popup_opened")
+  console.log(popPosition)
+  console.log(popUsername)
+  document.querySelector(".profile__username").textContent=popUsername
+  document.querySelector(".profile__position").textContent=popPosition
+});
+
+
