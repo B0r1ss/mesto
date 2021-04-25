6d3da6698f500d2f73451437735361ff7c79c862
Vladimir Borisenko <borisenko.va90@gmail.com>
2021-04-26 00:17:53 +0300

    refactorind/add card

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-const templateCard = document.querySelector("#card").content;
+import { Card } from "./Cards.js"
+
 const elementsList = document.querySelector(".elements__list");
 const profileButtonAdd = document.querySelector(".profile__button_add");
 const profileButtonEdit = document.querySelector(".profile__button_edit");
@@ -27,33 +28,20 @@ const position = document.querySelector(".profile__position");
 const popupImgImage = popupImg.querySelector(".popup__image");
 const popupImgTitle = popupImg.querySelector(".popup__title-image");
 
-/*CREATE CARD*/
-function createCard(obj) {
-  const cardPlace = templateCard.cloneNode(true);
-  const placeImage = cardPlace.querySelector(".place__image");
-  const likeButton = cardPlace.querySelector(".place__like-button");
-  const deleteButton = cardPlace.querySelector(".place__delete-button");
 
-  cardPlace.querySelector(".place__image").src = String(obj.link);
-  cardPlace.querySelector(".place__image").alt = String(obj.name);
-  cardPlace.querySelector(".place__title").textContent = String(obj.name);
-
-  placeImage.addEventListener("click", function (evt) {
-    openPopupImage(evt.target);
-  });
-  likeButton.addEventListener("click", function (evt) {
-    evt.target.classList.toggle("place__like-button_enable");
-  });
-  deleteButton.addEventListener("click", function (evt) {
-    placeDelete(evt.target);
-  });
-  return cardPlace;
+function openPopupImage(place) {
+  console.log("ddddddd", place)
+  const image = place.src;
+  const title = place.alt;
+  popupImgTitle.textContent = title;
+  popupImgImage.src = image;
+  openPopup(popupImg);
 }
-
 /*ADD CARDS FROM VAR'S */
 initialCards.forEach((item) => {
-  const card = createCard(item);
-  elementsList.append(card);
+  const card = new Card(item, "#card", openPopupImage());
+  const cardElement = card.generateCard()
+  elementsList.append(cardElement);
 });
 
 /*DEFINE FUNCTIONS FOR LISTENERS*/
@@ -83,13 +71,7 @@ function openPopupEdit() {
   inputPosition.value = position.textContent;
 }
 
-function openPopupImage(place) {
-  const image = place.src;
-  const title = place.alt;
-  popupImgTitle.textContent = title;
-  popupImgImage.src = image;
-  openPopup(popupImg);
-}
+
 
 /*SUBMIT POPUPS */
 function submitPopupEdit(evt) {
@@ -115,9 +97,6 @@ function submitPopupAdd(evt) {
   setButtonState(inputList, buttonElement, settingsValidate);
 }
 
-function placeDelete(place) {
-  place.closest(".place").remove();
-}
 
 /*ADD LISTENERS */
 profileButtonAdd.addEventListener("click", () => {
